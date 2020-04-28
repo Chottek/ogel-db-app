b825e5adb8f257b9a2e9b1f408421b6faf1fad2f
Chottek <49697145+Chottek@users.noreply.github.com>
2020-04-28 14:12:53 +0200

    Ran some tests

--- src/test/java/pl/fox/ogel_db/OgelDbApplicationTests.java
+++ src/test/java/pl/fox/ogel_db/OgelDbApplicationTests.java
@@ -1,13 +1,37 @@
 package pl.fox.ogel_db;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.fox.ogel_db.model.ProductionData;
+import pl.fox.ogel_db.service.ProductionService;
+
+import java.util.List;
 
 @SpringBootTest
 class OgelDbApplicationTests {
 
+
+    @Autowired
+    private ProductionService service;
+
+
     @Test
-    void contextLoads() {
+    void namesOfMachinesAreInOrder() {    // assertion that names gotten from database are in order
+        List<String> names = service.getNames();
+
+        assert(names.get(0).equals("4x2 brick mould"));
+        assert(names.get(1).equals("2x2 brick mould"));
+        assert(names.get(2).equals("3x2 brick mould"));
+    }
+
+    @Test
+    void givenDateEqualsDateFromDB(){   // assertion that value of date param is equal in all ProductionData objects
+        String date = "2018-01-01";
+        List<ProductionData> data = service.getTotalValueOf(date);
+
+        for (ProductionData pd: data)
+            assert(pd.getDate().equals(date));
     }
 
 }
