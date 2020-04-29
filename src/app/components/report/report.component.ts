c520722c6a49a61b3b579a38b7dc6dee2d7a78cf
Chottek <49697145+Chottek@users.noreply.github.com>
2020-04-29 03:57:46 +0200

    Adjusted get methods in service, cleaned up code from IDE warnings

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MachineService} from "../../services/machine.service";
-import {ActivatedRoute} from "@angular/router";
+import {MachineService} from '../../services/machine.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-report',
@@ -13,76 +13,74 @@ export class ReportComponent implements OnInit {
   public machineData;
   public runtimeData;
   public oeeData;
-  private param;
-  public current_date = new Date();
+  public param;
+  public currentDate = new Date();
   public dates;
 
   ngOnInit(): void {
     this.getDates();
-    this.param = this.route.snapshot.queryParams["date"];    // get date parameter value
-    this.current_date = new Date();     // get date for generate mark
+    this.param = this.route.snapshot.queryParams.date;    // get date parameter value
+    this.currentDate = new Date();     // get date for generate mark
 
     this.getMachineData(this.param);   // get data from
     this.getRuntimeData(this.param);   // machineService
-    this.getOEE(this.param);   // by param
+    this.getOee(this.param);           // by param
   }
 
   redirect(value) {
     if (this.param) {
       this.param = value;
-      let URLSplit = window.location.href.split('?');
+      const URLSplit = window.location.href.split('?');
       return window.location.href = URLSplit[0] + '?date=' + this.param;
     }
   }
 
-  getOEE(date : string){
-    this.machineService.getOEE(date).subscribe(
-      data => { this.oeeData = data },
-      err => { console.error(err)},
-      () => { console.log("done!")}
+  getOee(date: string){
+    this.machineService.getOee(date).subscribe(
+      data => { this.oeeData = data; },
+      err => { console.error(err); },
+      () => { console.log('done!'); }
     );
   }
 
   getDates(){
     this.machineService.getDates().subscribe(
-      data => { this.dates = data },
-      err => { console.error(err)},
-      () => { console.log("done!")}
+      data => { this.dates = data; },
+      err => { console.error(err); },
+      () => { console.log('done!'); }
     );
   }
 
   getMachineData(date: string){
     this.machineService.getTotalValueOf(date).subscribe(
-       data => { this.machineData = data},
-        err => { console.error(err)},
-      () => { console.log("done!")}
+       data => { this.machineData = data; },
+        err => { console.error(err); },
+      () => { console.log('done!'); }
     );
   }
 
   getRuntimeData(date: string){
     this.machineService.getRuntime(date).subscribe(
-      data => { this.runtimeData = data},
-      err => { console.error(err)},
-      () => { console.log("done!")}
+      data => { this.runtimeData = data; },
+      err => { console.error(err); },
+      () => { console.log('done!'); }
     );
   }
 
-  parseWarning(n : number){  //get number and return warning in string
-    switch(n){
-      case 0: return "GOOD";
-      case 1: return "WARNING";
-      case 2: return "FATAL!";
+  parseWarning(n: number){  // get number and return warning in string
+    switch (n){
+      case 0: return 'GOOD';
+      case 1: return 'WARNING';
+      case 2: return 'FATAL!';
     }
   }
 
-  parseColor(n : number){ //get number and return color for warning
-    switch(n){
-      case 0: return "#008000"; //Hex code for green
-      case 1: return "#FFA500"; //Hex code for orange
-      case 2: return "#FF0000"; //Hex code for red
+  parseColor(n: number){ // get number and return color for warning
+    switch (n){
+      case 0: return '#008000'; // Hex code for green
+      case 1: return '#FFA500'; // Hex code for orange
+      case 2: return '#FF0000'; // Hex code for red
     }
   }
-
-  //TODO: Add <select> to choose date
 
 }
--- src/app/services/machine.service.ts
+++ src/app/services/machine.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 const httpOptions = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
 
@@ -11,18 +11,18 @@ export class MachineService {
   constructor(private http: HttpClient) {}
 
   getTotalValueOf(date: string){    // get data from api -> production
-    return this.http.get('/server/api/production/machines?date=' + date);
+    return this.http.get('/server/api/production?date=' + date);
   }
 
   getRuntime(date: string){   // get data from api -> runtime
-    return this.http.get('/server/api/runtime/time?date=' + date);
+    return this.http.get('/server/api/runtime?date=' + date);
   }
 
   getDates(){    // get dates from api -> production
     return this.http.get('/server/api/production/dates');
   }
 
-  getOEE(date: string){
+  getOee(date: string){
     return this.http.get('/server/api/runtime/oee?date=' + date);
   }
 
